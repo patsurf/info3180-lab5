b40f2ac8d76c2b95cfaf7f28f433291fd2a77e5f
patricia surf <74924633+patsurf@users.noreply.github.com>
2022-03-01 00:53:43 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -6,13 +6,11 @@ from .config import Config
 app = Flask(__name__)
 app.config.from_object(Config)
 
-db = SQLAlchemy(app)
 
 # Flask-Login login manager
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
-
-# Instantiate Flask-Migrate library here
+db = SQLAlchemy(app)
 
 from app import views
